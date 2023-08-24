bcfe0b82c287c4f9a51d1cf177954cb834a3f81f
Ashar Rashid <ashar.rashid@infosys.com>
2023-08-24 17:23:38 +0530

    FEAT: added color changing + minor fixes

--- src/app/daily/daily.component.ts
+++ src/app/daily/daily.component.ts
@@ -78,12 +78,55 @@ export class DailyComponent implements OnInit, OnChanges {
       return;
     }
     if (i + 1 < this.wordMatrix.length) {
+      this.colorTheLetters(i);
       let nextWord = this.wordMatrix[i + 1][0];
       nextWord.reference.focus();
     }
   };
 
+  colorTheLetters = (i: number) => {
+    this.wordMatrix[i].forEach((elm: any, index: number) => {
+      switch (this.answerWord.search(elm.value.toUpperCase())) {
+        case index: {
+          elm.reference.style.backgroundColor = 'green';
+          elm.reference.style.color = 'black';
+          break;
+        }
+        case -1: {
+          elm.reference.style.backgroundColor = 'red';
+          elm.reference.style.color = 'black';
+          break;
+        }
+        default: {
+          elm.reference.style.backgroundColor = 'yellow';
+          elm.reference.style.color = 'black';
+          break;
+        }
+      }
+    });
+  };
+
   test = (i: number, j: number, event: any) => {
+    console.log(event);
+
+    console.log('cl', i, j, event.key);
+
+    if (this.wordMatrix[i][j].value.length > 1) {
+      if (j + 1 < this.wordMatrix[i].length) {
+        this.wordMatrix[i][j + 1].value = event.data;
+        this.wordMatrix[i][j + 1].reference.focus();
+        this.wordMatrix[i][j].value = this.wordMatrix[i][j].value.slice(0, 1);
+        this.wordMatrix[i][j].reference.value = this.wordMatrix[i][j].value;
+        return;
+      } else {
+        this.wordMatrix[i][j].value = this.wordMatrix[i][j].value.slice(0, 1);
+        this.wordMatrix[i][j].reference.value = this.wordMatrix[i][j].value;
+        return;
+      }
+    }
+
+    this.wordMatrix[i][j].value = event.data;
+
     switch (event.inputType) {
       case 'insertText': {
         if (j + 1 < this.wordMatrix[i].length) {
@@ -95,12 +138,23 @@ export class DailyComponent implements OnInit, OnChanges {
         if (j - 1 >= 0) {
           this.wordMatrix[i][j - 1].reference.focus();
         }
-
         break;
       }
     }
   };
-
+  backspaceClickCheck = (i: number, j: number, event: any) => {
+    console.log('backspc', i, j, this.wordMatrix[i][j].value?.length);
+    // event.stopPropagation();
+    // if (this.wordMatrix[i][j].value?.length === 0) {
+    //   console.log('backspc');
+    //   if (j - 1 >= 0) {
+    //     console.log('backspc');
+    //     this.wordMatrix[i][j - 1].reference.focus();
+    //     event.stopPropagation();
+    //     return;
+    //   }
+    // }
+  };
   bindInputToMatrix = (htmlElm: any, word: any) => {
     word.reference = htmlElm;
   };
