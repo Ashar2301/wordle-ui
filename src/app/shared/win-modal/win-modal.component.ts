4fcbd4f572ae2e526eb8fb09270ec4424b75d228
Ashar Rashid <ashar.rashid2301@gmail.com>
2023-08-28 23:57:20 +0530

    MODIFY: updated charts in win-modal + minor changes

--- src/app/shared/win-modal/win-modal.component.ts
+++ src/app/shared/win-modal/win-modal.component.ts
@@ -1,38 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import {DynamicDialogRef} from 'primeng/dynamicdialog';
-import {DynamicDialogConfig} from 'primeng/dynamicdialog';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-win-modal',
   templateUrl: './win-modal.component.html',
-  styleUrls: ['./win-modal.component.scss']
+  styleUrls: ['./win-modal.component.scss'],
 })
 export class WinModalComponent implements OnInit {
+  constructor(
+    public ref: DynamicDialogRef,
+    public config: DynamicDialogConfig
+  ) {}
 
-  constructor( public ref: DynamicDialogRef, public config: DynamicDialogConfig) { }
-
-  pieData:any = {};
+  pieData: any = {};
+  pieOptions: any = {};
+  hoursUntilNewWordle : number = 0;
+  minutesUntilNewWordle : number = 0;
   ngOnInit(): void {
     console.log(this.config.data?.attempt);
     this.pieData = {
-      labels: ['1','2','3','4','5','6'],
+      labels: ['1', '2', '3', '4', '5', '6'],
       datasets: [
-          {
-              data: [0, 0, 1,0, 0, 1],
-              backgroundColor: [
-                  "#42A5F5",
-                  "#66BB6A",
-                  "#FFA726"
-              ],
-              hoverBackgroundColor: [
-                  "#64B5F6",
-                  "#81C784",
-                  "#FFB74D"
-              ]
-          }
-      ]
-  };
-    
-  }
+        {
+          data: [1, 1, 1, 1, 1, 1],
+          backgroundColor: [
+            '#071426',
+            '#003F59',
+            '#00717C',
+            '#00A385',
+            '#82D279',
+            '#F9F871',
+          ],
+          hoverBackgroundColor: [
+            '#071426',
+            '#003F59',
+            '#00717C',
+            '#00A385',
+            '#82D279',
+            '#F9F871',
+          ],
+        },
+      ],
+    };
 
+    this.pieOptions = {
+      plugins: {
+        legend: {
+          position: 'left',
+          label: {
+            padding: '30',
+          },
+        },
+      },
+    };
+    const currentDate: Date = new Date();
+    const tempTomorrowDate: Date = new Date(
+      currentDate.getTime() + 24 * 60 * 60 * 1000
+    );
+    const tomorrowDate :Date = new Date(tempTomorrowDate.getFullYear() , tempTomorrowDate.getMonth() , tempTomorrowDate.getDate());
+
+    const timeRemaining: number =
+    tomorrowDate.getTime() - currentDate.getTime();
+    
+    this.hoursUntilNewWordle = Math.floor(timeRemaining / (1000 * 60 * 60));
+    this.minutesUntilNewWordle = Math.floor(
+      (timeRemaining / (1000 * 60)) % 60
+    );
+
+  }
 }
