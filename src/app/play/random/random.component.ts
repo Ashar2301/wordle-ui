65cb1f3120067723695501ed759619c847f21f9c
Ashar Rashid <ashar.rashid2301@gmail.com>
2023-09-07 23:15:44 +0530

    MODIFY : minor changes

--- src/app/play/random/random.component.ts
+++ src/app/play/random/random.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { DialogService } from 'primeng/dynamicdialog';
+import { WinModalComponent } from 'src/app/shared/win-modal/win-modal.component';
 
 @Component({
   selector: 'app-random',
   templateUrl: './random.component.html',
-  styleUrls: ['./random.component.scss']
+  styleUrls: ['./random.component.scss'],
+  providers: [DialogService],
 })
-export class RandomComponent implements OnInit {
+export class RandomComponent implements OnInit,OnChanges {
+  answerWord: string = 'SALET';
+  constructor(public dialogService: DialogService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
   }
 
+  evaluateResult=(result:boolean)=>{
+    if(result)
+    {
+      this.showWinModal();
+    }
+  }
+  showWinModal() {
+    const ref = this.dialogService.open(WinModalComponent, {
+      header: 'Congratulations',
+      width: '70vw',
+      height: '80vh',
+      data: {
+        attempt: 4,
+      },
+    });
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 }
